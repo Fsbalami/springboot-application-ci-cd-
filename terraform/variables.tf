28ff0f9e37809af57c07796a59c0c96f705cbd45
Francis <fsbalami05@gmail.com>
2025-09-08 02:22:02 +0100

    updated terraform code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,35 +5,35 @@ variable "region" {
 
 variable "vpc_id" {
   description = "The ID of the VPC where resources will be created"
-  default     = "vpc-08a20b564a94cad98"
+  default     = "vpc-02d6b1f39caf64fe8"
 }
 
 variable "instance_ami" {
   description = "AMI ID for the instances"
-  default     = "ami-0195204d5dce06d99"
+  default     = "ami-0a232144cf20a27a5"
 }
 
 variable "instance_key_name" {
   description = "An Existing Keypair to be used for the instances"
-  default     = "project-key"
+  default     = "Trainig-key-pair"
 }
 
 variable "instance_subnet_id" {
   description = "Public Subnet ID for the instances"
-  default     = "subnet-04e7a2e028dcb65ea"
+  default     = "subnet-08de206d5040430a8"
 }
 
 variable "instance_type" {
   description = "The Instance type"
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket for Terraform state"
-  default     = "stunnerz-springboot-app-bucket"
+  default     = "fsb-springboot-app-buckett"
 }
 
 variable "dynamodb_table_name" {
   description = "The name of the DynamoDB table for Terraform state locking"
-  default     = "stunnerz-springboot-app-DynamoDB"
+  default     = "fsb-springboot-app-DynamoDB"
 }
